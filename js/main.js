6e5340658390eecd8f955a5dc1c9a8f2e3c0b751
steven-bargery <sbargery@gmail.com>
2020-07-17 12:25:21 -0400

    added API GET request

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
   zooming.listen('.img-zoomable')
 })
+
+
+const headers = new Headers({
+    'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+});
+
+const request = new Request('https://4ptmnr3qf5.execute-api.us-east-1.amazonaws.com/production/visitors', {
+    headers: headers
+});
+
+fetch(request)
+.then(response => response.json())
+.then(jsonResponse => {
+  console.log(jsonResponse);
+});
